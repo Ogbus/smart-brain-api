22ed1188b4deaacd7e5d353d3e36af667d61615a
Ogbuluijah Owajiyagham <felixgbuluijah@yahoo.com>
2018-10-29 21:21:35 +0200

    fixing database

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const image = require('./Controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : 'process.env.DATABASE_URL',
-      ssl: true,
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
